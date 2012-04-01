dd7bf49117ed041b18a565756fce0c3895ee3da1
lin <lin900627@163.com>
2012-04-01 12:42:16 +0800

    add model Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401021051) do
+ActiveRecord::Schema.define(:version => 20120401025718) do
+
+  create_table "messages", :force => true do |t|
+    t.text     "content"
+    t.integer  "from"
+    t.integer  "to"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
