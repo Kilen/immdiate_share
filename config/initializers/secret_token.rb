24ef2980385a29470ce413c2c7518007ea1428fc
unknown <Administrator@S99Y0K6KONPI1NV.(none)>
2012-04-01 10:32:26 +0800

    add model User

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImmediateShare::Application.config.secret_token = '62fb5bef46e65b0ef548b738e71644548776269b1ca9266c8a6489e9365c2cdbdb90b220bf586f41ac8061c26759273dadea0111926d24d07b35b1b9dcd66bfa'
